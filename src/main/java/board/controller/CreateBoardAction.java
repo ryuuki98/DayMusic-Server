fa781c2a7dee5803653f5ca0f1d5c3a6ca883194
nooleee <wjssnfl7739@naver.com>
2024-05-22 18:09:55 +0900

    Create CreateBoardAction 생성

---
+++ src/main/java/board/controller/CreateBoardAction.java
@@ -0,0 +1,28 @@
+package board.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CreateBoardAction
+ */
+public class CreateBoardAction extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+//		System.out.println("데이터 수신 성공");
+		
+		
+		doGet(request, response);
+	}
+
+}
