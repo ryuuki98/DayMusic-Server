ee72f2e538ff6a24e9e9703dc9c8ae8057a37ab6
ryuuki98 <ryuuki98@naver.com>
2024-05-23 18:18:18 +0900

    modify: 닉네임 변경 액션 생성 및 완성

---
+++ src/main/java/user/controller/action/UpdateNicknameAction.java
@@ -0,0 +1,33 @@
+package user.controller.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import user.controller.Action;
+import user.model.UserDao;
+import user.model.UserResponseDto;
+
+public class UpdateNicknameAction implements Action {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		UserDao userDao = UserDao.getInstance();
+
+		String id = request.getParameter("id");
+		String newNickname = request.getParameter("nickname");
+		
+		System.out.println(id);
+		System.out.println(newNickname);
+
+		if (userDao.duplicateNickname(id,newNickname)) {
+			System.out.println("닉네임이 중복됩니다.");
+		} else {
+			UserResponseDto user = userDao.updateNickname(id, newNickname);
+		}
+
+	}
+
+}
