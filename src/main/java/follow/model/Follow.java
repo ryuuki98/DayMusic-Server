fb3a8043fd613ad5ed7fc8b5b96fbf9af94c7f45
mingikim-giv <mingikim.giv@gmail.com>
2024-05-23 16:22:49 +0900

    Modify: Follow Class(getter)

--- src/main/java/follow/model/Follow.java
+++ src/main/java/follow/model/Follow.java
@@ -1,48 +1,40 @@
 package follow.model;
 
+import java.sql.Timestamp;
+
 public class Follow {
 	private int followCode;
 	private String followedId;
 	private String followerId;
-	private String nickname;
+	private Timestamp regDate;
+	private Timestamp modDate;
 	
-	public Follow(int followCode, String followedId, String followerId, String nickname) {
+	public Follow(int followCode, String followedId, String followerId, Timestamp regDate, Timestamp modDate) {
 		super();
 		this.followCode = followCode;
 		this.followedId = followedId;
 		this.followerId = followerId;
-		this.nickname = nickname;
-	}
-	
-	public int getFollowCode() {
-		return followCode;
+		this.regDate = regDate;
+		this.modDate = modDate;
 	}
 
-	public void setFollowCode(int followCode) {
-		this.followCode = followCode;
+	public int getFollowCode() {
+		return followCode;
 	}
 
 	public String getFollowedId() {
 		return followedId;
 	}
 
-	public void setFollowedId(String followedId) {
-		this.followedId = followedId;
-	}
-
 	public String getFollowerId() {
 		return followerId;
 	}
 
-	public void setFollowerId(String followerId) {
-		this.followerId = followerId;
+	public Timestamp getRegDate() {
+		return regDate;
 	}
 
-	public String getNickname() {
-		return nickname;
-	}
-
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
+	public Timestamp getModDate() {
+		return modDate;
 	}
 }
