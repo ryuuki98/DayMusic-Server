9120894678f240555d8683f785c8af125856f9f5
nooleee <wjssnfl7739@naver.com>
2024-05-23 12:52:26 +0900

    Add findBaordById, createBoard 메소드 추가

--- src/main/java/board/module/BoardDao.java
+++ src/main/java/board/module/BoardDao.java
@@ -1,5 +1,83 @@
 package board.module;
 
-public class BoardDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
+import util.DBManager;
+
+public class BoardDao {
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	private BoardDao() {
+		
+	}
+	
+	private static BoardDao instance = new BoardDao();
+	
+	public static BoardDao getInstance() {
+		return instance;
+	}
+
+	
+	// 
+	public BoardResponseDto findBoardById(String userId) {
+		BoardResponseDto board = null;
+		
+		try {
+			conn = DBManager.getConnection();
+			
+			String sql = "SELECT id, contents, music_code, board_code, reg_date, mod_date, is_public FROM board WHERE id=?";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userId);
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				String contents = rs.getString(2);
+				String musicCode = rs.getString(3);
+				int boardCode = rs.getInt(4);
+				java.sql.Timestamp reg_date = rs.getTimestamp(5);
+				java.sql.Timestamp mod_date = rs.getTimestamp(6);
+				int isPublic = rs.getInt(7);
+				
+				board = new BoardResponseDto(boardCode, userId, contents, musicCode, isPublic, reg_date, mod_date);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return board;
+	}
+	
+	public BoardResponseDto createBoard(BoardRequestDto boardDto) {
+		
+		try {
+			conn = DBManager.getConnection();
+			String sql = "INSERT INTO board(id, contents, is_public) VALUES(?, ?, ?)";
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, boardDto.getId());
+			pstmt.setString(2, boardDto.getContents());
+//			pstmt.setString(3, boardDto.getMusicCode());
+			pstmt.setInt(3, boardDto.isPublic());
+			
+			pstmt.execute();
+			
+			return findBoardById(boardDto.getId());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, pstmt);
+		}
+		
+		return null;
+	}
 }
