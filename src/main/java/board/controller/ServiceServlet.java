880eaa28e40c54a4ee2af2acef3186a8cbe3d8d8
nooleee <wjssnfl7739@naver.com>
2024-05-26 17:42:47 +0900

    Add 콘솔 확인용 추가

--- src/main/java/board/controller/ServiceServlet.java
+++ src/main/java/board/controller/ServiceServlet.java
@@ -1,11 +1,14 @@
 package board.controller;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
 
 
 public class ServiceServlet extends HttpServlet {
@@ -15,14 +18,33 @@ public class ServiceServlet extends HttpServlet {
 		// ㄴ 생성패턴 싱글톤 , 팩토리메소드 , 커맨드 
 		
 		//input hidden 으로 넘겨도 되고 경로에 ?command = "ddd" 로 넘겨도 되고 
-		String command = request.getParameter("command");
-		System.out.println("command : " + command);
+//		String command = request.getParameter("command");
+//		String contents = request.getParameter("contents");
+//        String isPublic = request.getParameter("isPublic");
+//		System.out.println("command : " + command);
+//		System.out.println("contents : " + contents);
+//		System.out.println("ispublic : " + isPublic);
+		
+		request.setCharacterEncoding("UTF-8");
+		StringBuilder sb = new StringBuilder();
+		BufferedReader reader = request.getReader();
+		String line;
+		while ((line = reader.readLine()) != null) {
+			sb.append(line);
+		}
+		System.out.println(sb.toString());
+		JSONObject jsonObject = new JSONObject(sb.toString());
+		
+		
+		// JSON 데이터 추출
+		String command = jsonObject.getString("command");
 		
 		if(command != null) {
 			BoardActionFactory af = BoardActionFactory.getInstance();
 			BoardAction action = af.getAction(command);
 			
 			if(action != null) {
+				request.setAttribute("data", jsonObject);
 				action.execute(request, response);
 			} else {
 				response.sendError(404);
