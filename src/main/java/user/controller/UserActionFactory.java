2c7dc19d8c7f7c8bd217eff700e7bb95b26fa9cb
ryuuki98 <ryuuki98@naver.com>
2024-05-26 15:17:25 +0900

    modify: CORSfilter추가 , action, actionFactory, ServiceServlet 파일명 변경

--- src/main/java/user/controller/UserActionFactory.java
+++ src/main/java/user/controller/UserActionFactory.java
@@ -1,23 +1,26 @@
 package user.controller;
 
+import user.controller.action.DeleteUserAction;
 import user.controller.action.JoinAction;
 import user.controller.action.LoginAction;
+import user.controller.action.UpdateInformation;
 import user.controller.action.UpdateNicknameAction;
 import user.controller.action.UpdatePasswordAction;
+import user.controller.action.UpdateProfileImage;
 
-public class ActionFactory {
-	private ActionFactory() {
+public class UserActionFactory {
+	private UserActionFactory() {
 		
 	}
 	
-	private static ActionFactory instance = new ActionFactory();
+	private static UserActionFactory instance = new UserActionFactory();
 	
-	public static ActionFactory getInstance() {
+	public static UserActionFactory getInstance() {
 		return instance;
 	}
 	
-	public Action getAction(String command) {
-		Action action = null;
+	public UserAction getAction(String command) {
+		UserAction action = null;
 		
 		if (command.equals("login")) {
 			action = new LoginAction();
@@ -27,6 +30,12 @@ public class ActionFactory {
 			action = new UpdateNicknameAction();
 		}else if (command.equals("updatePassword")) {
 			action = new UpdatePasswordAction();
+		}else if (command.equals("updateProfileImage")) {
+			action = new UpdateProfileImage();
+		}else if (command.equals("updateInformation")) {
+			action = new UpdateInformation();
+		}else if (command.equals("deleteUser")) {
+			action = new DeleteUserAction();
 		}
 		return action;
 		
--- src/main/java/user/controller/UserServiceServlet.java
+++ src/main/java/user/controller/UserServiceServlet.java
@@ -1,5 +1,6 @@
 package user.controller;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,30 +8,33 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ServiceServlet extends HttpServlet {
+import org.json.JSONObject;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-	}
+public class UserServiceServlet extends HttpServlet {
+
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		String command = request.getParameter("command");
-		System.out.println(command);
+		StringBuilder sb = new StringBuilder();
+		BufferedReader reader = request.getReader();
+		String line;
+		while ((line = reader.readLine()) != null) {
+			sb.append(line);
+		}
+		JSONObject jsonObject = new JSONObject(sb.toString());
 
-		ActionFactory af = ActionFactory.getInstance();
-		Action action = af.getAction(command);
+		// JSON 데이터 추출
+		String command = jsonObject.getString("command");
+		System.out.println(command);
+		UserActionFactory af = UserActionFactory.getInstance();
+		UserAction action = af.getAction(command);
 
 		if (action != null) {
+			request.setAttribute("jsonRequest", jsonObject);
 			action.execute(request, response);
 		}
-		doGet(request, response);
 	}
 
-	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		doGet(request, response);
-	}
 
 }
