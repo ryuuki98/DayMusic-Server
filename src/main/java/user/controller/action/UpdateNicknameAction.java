9eff4166d67c9bde4dc8735fbc6d7c37cc121890
ryuuki98 <ryuuki98@naver.com>
2024-05-27 18:13:08 +0900

    modify: UpdateNicknameAction json형식으로 변경

--- src/main/java/user/controller/action/UpdateNicknameAction.java
+++ src/main/java/user/controller/action/UpdateNicknameAction.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.json.JSONObject;
 import user.controller.UserAction;
 import user.model.UserDao;
 import user.model.UserResponseDto;
@@ -16,16 +18,29 @@ public class UpdateNicknameAction implements UserAction {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		UserDao userDao = UserDao.getInstance();
 
-		String id = request.getParameter("id");
-		String newNickname = request.getParameter("nickname");
-		
-		System.out.println(id);
-		System.out.println(newNickname);
+		JSONObject jsonObject = (JSONObject) request.getAttribute("jsonRequest");
+
+		System.out.println(jsonObject.toString());
+
+		String id = jsonObject.getString("id");
+		String newNickname = jsonObject.getString("nickname");
+
+
 
 		if (userDao.duplicateNickname(id,newNickname)) {
-			System.out.println("닉네임이 중복됩니다.");
+			System.out.println("닉네임 중복, 변경 실패");
+			response.setStatus(HttpServletResponse.SC_OK);
+			response.getWriter().write("{\"message\":\"Nickname update failure\"}");
 		} else {
 			UserResponseDto user = userDao.updateNickname(id, newNickname);
+			System.out.println("닉네임 변경 완료");
+
+			HttpSession session = request.getSession();
+
+			session.setAttribute("nickname", user.getNickname());
+			System.out.println(user.getNickname());
+			response.setStatus(HttpServletResponse.SC_OK);
+			response.getWriter().write("{\"message\":\"Nickname update successful\"}");
 		}
 
 	}
