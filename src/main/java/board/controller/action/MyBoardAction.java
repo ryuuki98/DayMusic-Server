00114794712c33b15118f6f6154132246221e6b1
nooleee <wjssnfl7739@naver.com>
2024-05-28 18:26:09 +0900

    Create MyBoardAction 추가

---
+++ src/main/java/board/controller/action/MyBoardAction.java
@@ -0,0 +1,65 @@
+package board.controller.action;
+
+import board.controller.BoardAction;
+import board.module.BoardDao;
+import board.module.BoardResponseDto;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MyBoardAction  extends HttpServlet implements BoardAction  {
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+//        System.out.println("Request attributes: " + request.getAttributeNames());
+        JSONObject jsonObject = (JSONObject) request.getAttribute("data");
+        String userId = jsonObject.getString("id");
+        BoardDao boardDao = BoardDao.getInstance();
+        List<BoardResponseDto> boardList = boardDao.findBoardMyID(userId);
+
+        System.out.println("boardList : " + boardList);
+//        for(int i =0; i < boardList.size(); i++){
+//            BoardResponseDto board = boardList.get(i);
+//            System.out.println("board : " + board);
+//        }
+
+//        System.out.println("Total boards fetched: " + boardList.size());
+
+//        List<BoardResponseDto> filteredBoardList = boardList.stream()
+//                .collect(Collectors.toList());
+//
+//        System.out.println("Total public boards: " + filteredBoardList.size());
+
+        JSONObject jsonResponse = new JSONObject();
+        jsonResponse.put("status", 200);
+
+        JSONArray boardArray = new JSONArray();
+        for (BoardResponseDto board : boardList) {
+            JSONObject boardJson = new JSONObject();
+            boardJson.put("id", board.getId());
+            boardJson.put("contents", board.getContents());
+            boardJson.put("board_code", board.getBoardCode());
+            boardJson.put("music_code", board.getMusicCode());
+            boardJson.put("is_public", board.isPublic());
+            boardArray.put(boardJson);
+        }
+
+        jsonResponse.put("boardList", boardArray);
+        response.setStatus(HttpServletResponse.SC_OK);
+        System.out.println("JSON Response: " + jsonResponse.toString());
+
+        PrintWriter out = response.getWriter();
+
+        out.print(jsonResponse.toString());
+        out.flush();
+    }
+}
