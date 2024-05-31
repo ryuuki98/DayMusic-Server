254c6656e00551f1fb632df6e9995e95add2a611
nooleee <wjssnfl7739@naver.com>
2024-05-31 12:39:15 +0900

    Add 댓글 작성 메소드 추가

--- src/main/java/comment/controller/action/CommentAddAction.java
+++ src/main/java/comment/controller/action/CommentAddAction.java
@@ -1,4 +1,57 @@
 package comment.controller.action;
 
-public class CommentAddAction {
+import comment.controller.CommentAction;
+import comment.module.Comment;
+import comment.module.CommentDao;
+import org.json.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class CommentAddAction extends HttpServlet implements CommentAction {
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+
+        // JSON 객체 가져오기
+        JSONObject jsonObject = (JSONObject) request.getAttribute("data");
+
+        String userId = jsonObject.getString("id");
+        String contents = jsonObject.getString("contents");
+        int boardCode = jsonObject.getInt("boardCode");
+        int parent = jsonObject.has("parent") ? jsonObject.getInt("parent") : 0;
+
+        boolean isValid = contents != null && !contents.isEmpty();
+
+        response.setContentType("text/plain; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        if (isValid) {
+            Comment comment = new Comment(userId, boardCode, contents, parent);
+
+            CommentDao commentDao = CommentDao.getInstance();
+            boolean isSuccess = commentDao.addComment(comment);
+
+            if (isSuccess) {
+                response.setStatus(HttpServletResponse.SC_OK);
+                System.out.println("댓글 작성 완료");
+                out.print("댓글 작성 완료");
+            } else {
+                response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                System.out.println("댓글 작성 실패");
+                out.print("댓글 작성 실패");
+            }
+        } else {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST); // 400 상태 코드 설정
+            System.out.println("유효하지 않은 입력");
+            out.print("유효하지 않은 입력");
+        }
+
+        out.flush();
+        out.close();
+    }
 }
