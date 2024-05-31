b39a6983f135ac15e46b20e4d94b5910373eebe1
nooleee <wjssnfl7739@naver.com>
2024-05-31 12:39:23 +0900

    Add 댓글 삭제 메소드 추가

--- src/main/java/comment/controller/action/CommentDeleteAction.java
+++ src/main/java/comment/controller/action/CommentDeleteAction.java
@@ -1,4 +1,43 @@
 package comment.controller.action;
 
-public class CommentDeleteAction {
+import comment.controller.CommentAction;
+import comment.module.CommentDao;
+import org.json.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class CommentDeleteAction extends HttpServlet implements CommentAction {
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+
+        // JSON 객체 가져오기
+        JSONObject jsonObject = (JSONObject) request.getAttribute("data");
+
+        int cmtCode = jsonObject.getInt("cmtCode");
+
+        response.setContentType("text/plain; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        CommentDao commentDao = CommentDao.getInstance();
+        boolean isSuccess = commentDao.deleteComment(cmtCode);
+
+        if (isSuccess) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            System.out.println("댓글 삭제 완료");
+            out.print("댓글 삭제 완료");
+        } else {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            System.out.println("댓글 삭제 실패");
+            out.print("댓글 삭제 실패");
+        }
+
+        out.flush();
+        out.close();
+    }
 }
