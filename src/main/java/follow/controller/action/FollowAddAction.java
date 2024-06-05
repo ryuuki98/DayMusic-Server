e86e9c7aa407911a824b52f388da07244b6428c1
mingikim-giv <mingikim.giv@gmail.com>
2024-06-05 18:50:43 +0900

    Modify: FollowAddAction

--- src/main/java/follow/controller/action/FollowAddAction.java
+++ src/main/java/follow/controller/action/FollowAddAction.java
@@ -15,14 +15,20 @@ import org.json.JSONObject;
 public class FollowAddAction extends HttpServlet implements FollowAction {
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
 		request.setCharacterEncoding("UTF-8");
 
 		JSONObject jsonResponse = new JSONObject();
 		JSONObject jsonObject = (JSONObject) request.getAttribute("jsonRequest");
 
 		FollowDao followDao = FollowDao.getInstance();
+
 		String followerId = jsonObject.getString("followerId");
 		String followedId = jsonObject.getString("followedId");
+
+		boolean isFollow = followDao.isFollowing(followerId, followedId);
+		System.out.println("팔로우 추가 : " + isFollow);
+		jsonObject.put("isFollow", isFollow);
 
 		String message = followDao.followChange(followerId, followedId);
 		jsonResponse.put("message", message);
